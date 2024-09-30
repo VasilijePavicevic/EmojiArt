542210ec147faec6b2e708d14917730dac3f004d
VasilijePavicevic <vasilijepavicevic4@gmail.com>
2024-09-30 17:08:09 +0200

    Komit za pr

--- Emoji Art/EmojiArtDocumentView.swift
+++ Emoji Art/EmojiArtDocumentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct EmojiArtDocumentView: View {
+    typealias Emoji = EmojiArt.Emoji
     @ObservedObject var document: EmojiArtDocument
     
     private let emojis = "🏓🪀🏀🎱🥏😀🤓👺🐶🐱🐭🐹🍏🥙🚗✈️🚁🛫🛩️🥂🍥🍩🍚🍒🤹‍♂️🌝🌼🌞🐬🐥🐝💧⛄️"
@@ -17,12 +18,11 @@ struct EmojiArtDocumentView: View {
     var body: some View {
         VStack(spacing: 0){
             documentBody
-            Color.yellow
             ScrollingEmojis(emojis)
                 .font(.system(size: paletteEmojiSize))
                 .padding(.horizontal)
                 .scrollIndicators(.hidden)
-
+            
         }
     }
     
@@ -30,13 +30,76 @@ struct EmojiArtDocumentView: View {
         GeometryReader { geometry in
             ZStack{
                 Color.white
-                ForEach(document.emojis) { emoji in
-                    Text(emoji.string)
-                        .font(emoji.font)
-                        .position(emoji.position.in(geometry))
-                }
+                documentContents(in: geometry)
+                    .scaleEffect(zoom * gestureZoom)
+                    .offset(pan + gesturePan)
+            }
+            .gesture(panGesture.simultaneously(with: zoomGesture))
+            .dropDestination(for: Sturldata.self) { sturldatas, location in
+                return drop(sturldatas, at: location, in: geometry)
             }
         }
+    }
+    
+    @State private var zoom: CGFloat = 1
+    @State private var pan: CGOfsett = .zero
+    
+    @GestureState private var gestureZoom: CGFloat = 1
+    @GestureState private var gesturePan: CGOfsett = .zero
+    
+    private var zoomGesture: some Gesture {
+        MagnificationGesture()
+            .updating($gestureZoom) { inMotionPinchScale, gestureZoom, _ in
+                gestureZoom = inMotionPinchScale
+            }
+            .onEnded { endingPinchScale in
+                zoom *= endingPinchScale
+            }
+    }
+    
+    private var panGesture: some Gesture {
+        DragGesture()
+            .updating($gesturePan) { value, gesturePan, _ in
+                gesturePan = value.translation
+            }
+            .onEnded { value in
+                pan += value.translation
+            }
+    }
+    
+    @ViewBuilder
+    private func documentContents(in geometry: GeometryProxy) -> some View {
+        AsyncImage(url: document.background)
+            .position(Emoji.Position.zero.in(geometry))
+        ForEach(document.emojis) { emoji in
+            Text(emoji.string)
+                .font(emoji.font)
+                .position(emoji.position.in(geometry))
+        }
+        
+    }
+    
+    private func drop(_ sturldatas: [Sturldata], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
+        for sturldata in sturldatas{
+            switch sturldata {
+            case .url(let url):
+                document.setBackground(url)
+                return true
+            case .string(let emoji):
+                document.addEmoji(
+                    emoji,
+                    at: emojiPosition(at: location, in: geometry),
+                    size: paletteEmojiSize / zoom
+                )
+                return true
+            default: break
+            }
+        }
+        return false
+    }
+    private func emojiPosition(at location: CGPoint, in geometry: GeometryProxy) -> Emoji.Position {
+        let center = geometry.frame(in: .local).center
+        return Emoji.Position(x: Int((location.x - center.x - pan.width) / zoom), y: Int(-(location.y - center.y - pan.height) / zoom))
     }
 }
 
@@ -54,6 +117,7 @@ struct ScrollingEmojis: View {
             HStack{
                 ForEach(emojis, id: \.self) { emoji in
                     Text(emoji)
+                        .draggable(emoji)
                 }
             }
         }
---
+++ Emoji Art/Palette.swift
@@ -0,0 +1,16 @@
+//
+//  Palette.swift
+//  Emoji Art
+//
+//  Created by Vasilije Pavicevic on 30.9.24..
+//
+
+import Foundation
+
+struct Palette: Identifiable {
+    var name: String
+    var emojis: String
+    let id = UUID()
+    
+    
+}
